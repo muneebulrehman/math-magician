0045a69862cf5385702dc2c49acf85e51dfb825a
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-16 15:50:14 +0530

    Added event handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Calculator from './components/Calculator';
-import Calculate from './logic/calculate';
-import operate from './logic/operate'
 
 export default class App extends React.Component {
   constructor(props) {
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Calculate from '../logic/calculate';
 import './Calculator.css';
 
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
+    if (!e.target.name) return;
+    const { total, next, operation } = Calculate(this.state, e.target.name);
+    if (!next && !total) this.setState({ total: '0', next, operation });
+    else this.setState({ total, next, operation });
   }
 
   render() {
@@ -30,12 +43,17 @@ export default class Calculator extends React.Component {
       '=',
     ];
     const operations = '+-x÷=';
+    const { total, next } = this.state;
     return (
       <section className="calculator">
-        <div className="input">0</div>
+        {total ? (
+          <div className="input">{total}</div>
+        ) : (
+          <div className="input">{next}</div>
+        )}
         <div className="buttons">
           {btns.map((btn) => (
-            <button type="button" key={btn} id={btn} className={`btn ${operations.includes(btn) ? 'operations' : ''} ${btn === '0' ? 'span-large' : ''}`}>
+            <button type="button" key={btn} name={btn} className={`btn ${operations.includes(btn) ? 'operations' : ''} ${btn === '0' ? 'span-large' : ''}`} onClick={this.handleClick}>
               {btn}
             </button>
           ))}
