d3e55e71e0babd8c647ed353a1f4e6b792c4a102
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-16 15:16:27 +0530

    added provided files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Calculator from './components/Calculator';
+import Calculate from './logic/calculate';
+import operate from './logic/operate'
 
 export default class App extends React.Component {
   constructor(props) {
